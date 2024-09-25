6a1b0d0de9e87f6e602463c9b8e1d1123473ca24
ASUS <ASUS@LAPTOP-PGERUNON>
2024-09-25 17:17:28 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+class Program
+{
+    static async Task Main(string[] args)
+    {
+        Console.Write("hay nhap dia chi IP: ");
+        string ipAddress = Console.ReadLine();
+        var client = new HttpClient();
+        var request = new HttpRequestMessage(HttpMethod.Get, $"http://ipinfo.io/{ipAddress}/geo");
+        var response = await client.SendAsync(request);
+        response.EnsureSuccessStatusCode();
+        Console.WriteLine(await response.Content.ReadAsStringAsync());
+    }
+}
